295ba618327c893d3074b7c36308d655f287f358
m3m3x <nicholasobri+github@gmail.com>
2025-03-03 11:08:36 -0500

    Collision and Signal

--- scripts/Character_Movement.gd
+++ scripts/Character_Movement.gd
@@ -1,12 +1,35 @@
 extends CharacterBody2D
 
-@export var speed : float = 10.0
+@export var speed : float = 1.0
+
+#VARIABLE ARE AS FOLLOWS:
+#var NAME : TYPE = VALUE
+#YOU DONT ALWAYS HAVE TO PASS A VALUE TO YOUR VARIABLES
+var can_interact : bool = false
+signal interact(obj_interact)
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("up"):
+		velocity.y += -speed
+	elif Input.is_action_pressed("down"):
+		velocity.y += speed
+	elif Input.is_action_pressed("left"):
+		velocity.x += -speed
+	elif Input.is_action_pressed("right"):
+		velocity.x += speed
+	else:
+		velocity = Vector2.ZERO
+	
+	move_and_slide()
+	
+	if can_interact and Input.is_action_just_pressed("interact"):
+		interact.emit()
+		
+	
--- scripts/demoStart.gd
+++ scripts/demoStart.gd
@@ -6,9 +6,24 @@ var main_char = load("res://scenes/Character.tscn")
 func _ready():
 	var m_c = main_char.instantiate()
 	add_child(m_c)
+	m_c.interact.connect(_on_interact.bind($Zone))
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_zone_entered(body):
+	if body is CharacterBody2D:
+		body.can_interact = true
+	pass
+
+func _on_interact(node):
+	node.visible = false
+
+
+func _on_zone_exited(body):
+	if body is CharacterBody2D:
+		body.can_interact = false
+	pass # Replace with function body.
